7f359052cfc84f8ebfab350eca55749cd0577590
mkthebea <5596molly@naver.com>
2023-07-04 21:32:56 +0900

    feat: presentation pages flow (#1)

--- src/pages/presentation/Speech.js
+++ src/pages/presentation/Speech.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Script from "./Script";
+import { Link } from "react-router-dom";
 
 const Speech = () => {
   return (
     <>
       <h1>Speech / 스크립트(피드백) 화면</h1>
       <Script />
+      <Link to="/presentation/practice">연습 시작</Link>
     </>
   );
 };
