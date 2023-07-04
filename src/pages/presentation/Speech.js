d32acf78d7d8d7f9c8f04c80e6e19a0e4bb328b7
mkthebea <5596molly@naver.com>
2023-07-04 19:15:21 +0900

    feat: create layout for presentation page (#1)

---
+++ src/pages/presentation/Speech.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Script from "./Script";
+
+const Speech = () => {
+  return (
+    <>
+      <h1>Speech / 스크립트(피드백) 화면</h1>
+      <Script />
+    </>
+  );
+};
+
+export default Speech;
---
+++ src/pages/presentation/Summary.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Summary = () => {
+  return <h1>Summary / 프레젠테이션 요약 </h1>;
+};
+
+export default Summary;
