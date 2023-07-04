c6e58acba781203fc243f9344f6787525733feef
mkthebea <5596molly@naver.com>
2023-07-04 22:09:03 +0900

    chore: design landing page

--- client/src/component/LandingPage.js
+++ client/src/component/LandingPage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import landing_image from "../image/landing_image.png";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#F38025",
+        overflow: "hidden",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <img src={landing_image} alt="landing" />
+    </div>
+  );
 };
 
 export default LandingPage;
