930129c54a5515a79b802a9e32810396eb002088
mkthebea <5596molly@naver.com>
2023-07-04 20:02:31 +0900

    feat: create layout for login and signin page (#1)

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ import NewPresentation from "./component/presentation/NewPresentation";
 import CoachList from "./component/coach/CoachList";
 import Portfolio from "./component/coach/Portfolio";
 
+import Login from "./component/login/Login";
+import SignIn from "./component/signin/SignIn";
+import SignInCoach from "./component/signin/SignInCoach";
+
 import Notfound from "./component/Notfound";
 
 function App() {
@@ -25,8 +29,13 @@ function App() {
         <Route path="/presentation/speech" element={<Speech />} />
         <Route path="/presentation/practice" element={<Practice />} />
         <Route path="/presentation/new" element={<NewPresentation />} />
+
         <Route path="/coach" element={<CoachList />} />
         <Route path="/coach/portfolio" element={<Portfolio />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signin/coach" element={<SignInCoach />} />
 
         <Route path="/*" element={<Notfound />} />
       </Routes>
