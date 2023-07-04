35fd3d9bbbcdf52d97f68e7b7dcb3b1071bc58d6
mkthebea <5596molly@naver.com>
2023-07-04 19:15:21 +0900

    feat: create layout for presentation page (#1)

---
+++ client/src/component/presentation/NewPresentation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPresentation = () => {
+  return <h1>NewPresentation / 프레젠테이션 생성 </h1>;
+};
+
+export default NewPresentation;
---
+++ client/src/component/presentation/Practice.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Script from "./Script";
+
+const Practice = () => {
+  return (
+    <>
+      <h1>Practice / 새 녹음 화면 or 연습 화면</h1>
+      <Script />
+    </>
+  );
+};
+
+export default Practice;
