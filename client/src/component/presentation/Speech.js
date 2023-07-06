a970c161185e5a8f649acefd63ff8dcc29abc793
mkthebea <5596molly@naver.com>
2023-07-06 15:05:31 +0900

    feat: create draggable tools  and tool bar

--- client/src/component/presentation/Speech.js
+++ client/src/component/presentation/Speech.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Script from "./Script";
+import ToolBar from "./ToolBar";
 import { Link } from "react-router-dom";
 
 const Speech = () => {
   return (
     <>
       <h1>Speech / 스크립트(피드백) 화면</h1>
-      <Script />
+      <div style={{ display: "flex" }}>
+        <ToolBar />
+        <Script />
+      </div>
       <Link to="/presentation/practice">연습 시작</Link>
     </>
   );
