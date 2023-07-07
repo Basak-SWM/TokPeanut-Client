0eb1694005b6b9e08e69e084373fe20f6c954a8f
mkthebea <5596molly@naver.com>
2023-07-07 17:04:02 +0900

    feat: add highlights, erase, and interaction effects

--- src/component/presentation/Speech.js
+++ src/component/presentation/Speech.js
@@ -3,7 +3,6 @@ import Script from "./Script";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-import peanut_cursor from "../../image/peanut_cursor.png";
 import highlight from "../../image/icons/highlight.png";
 import faster from "../../image/icons/faster.png";
 import slower from "../../image/icons/slower.png";
@@ -12,15 +11,17 @@ import enter from "../../image/icons/enter.png";
 import pause from "../../image/icons/pause.png";
 import mouse from "../../image/icons/mouse.png";
 import slash from "../../image/icons/slash.png";
+import erase from "../../image/icons/erase.png";
 
 const Container = styled.div`
-  cursor: url(${(props) => props.cursor}) 25 25, grab;
+  cursor: url(${(props) => props.cursor}) 15 15, auto;
   display: flex;
 `;
 
+// 툴바
 const Tools = styled.div`
   width: 100px;
-  height: 500px;
+  height: 550px;
   border: 1px solid grey;
   border-radius: 5px;
   margin: 20px;
@@ -31,42 +32,63 @@ const Tools = styled.div`
   box-shadow: 2px 3px 5px 0px grey;
 `;
 
-const Tool = styled.div`
+// 툴바의 기호(hover effect)
+const ToolKit = styled.div`
   width: 30px;
   height: 30px;
   background-image: url(${(props) => props.src});
   background-size: cover;
-  cursor: url(${(props) => props.cursor}) 25 25, grab;
+  cursor: url(${(props) => props.cursor}) 25 25, auto;
 
   &:hover {
     width: 40px;
     height: 40px;
   }
-  // &:active,
-  // &:focus {
-  //   width: 30px;
-  //   height: 30px;
-  //   // cursor: url(${peanut_cursor}) 10 10, grab;
-  // }
 `;
 
+// 스크립트의 기호
+const Tool = styled.span`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  cursor: url(${(props) => props.cursor}) 25 25, auto;
+`;
+
+// 재생된 스크립트
 const PlayedText = styled.span`
   color: orange;
+  background-color: ${(props) => props.color};
   display: flex;
+  flex-direction: row;
   margin-right: 5px;
+
+  &:hover {
+    text-decoration: green wavy underline;
+  }
 `;
 
+// 재생 전 스크립트
 const Text = styled.span`
   color: black;
-  display: flex;
+  background-color: ${(props) => props.color};
   margin-right: 5px;
+  border-radius: 5px;
+
+  &:hover {
+    text-decoration: green wavy underline;
+  }
 `;
 
 const ScriptContainer = styled.div`
   width: 70vw;
+  height: 50vh;
   overflow-y: scroll;
   font-size: 24px;
-  display: flex;
+  padding: 24px;
+  border: 1px solid grey;
+  border-radius: 10px;
 `;
 
 // custom hook
@@ -101,30 +123,37 @@ const Speech = () => {
   // tool bar
   const [cursor, setCursor] = useState("");
   const [selectedSymbol, setSelectedSymbol] = useState(NaN); // 커서 관리를 위한 현재 선택된 기호 인덱스
-  const symbols = [highlight, faster, slower, edit, enter, pause, mouse, slash];
+  const symbols = [
+    highlight,
+    faster,
+    slower,
+    edit,
+    enter,
+    pause,
+    mouse,
+    slash,
+    erase,
+  ];
 
   // 기호 클릭시 selectedSymbol을 해당 기호 이미지로 변경 -> 커서 변경
   // 한 번 더 클릭시 기본 커서로 변경
   const clickTool = (e) => {
     const selectedSymbolIdx = e.target.id;
-    // if (selectedSymbol === "") {
-    //   setSelectedSymbol(selectedSymbolIdx);
-    //   console.log(selectedSymbolIdx);
-    // } else {
-    //   setSelectedSymbol("");
-    // }
     if (selectedSymbol) {
       setSelectedSymbol(NaN);
     } else {
       setSelectedSymbol(selectedSymbolIdx);
-      // console.log(selectedSymbolIdx);
     }
     // 커서 변경
     selectedSymbol ? setCursor("") : setCursor(symbols[selectedSymbolIdx]);
   };
 
   // script
-  const text = "저희는 음성 데이터 분석을 통해".split(" ");
+  const text =
+    "저희는 음성 데이터 분석을 통해 스피치에 있어 중요한 요소들을 평가하고, 연습에 유용한 도구를 제공하여 반복을 통한 스피치 실력 향상을 도우며, 스피치 전문가와의 부담 없는 코칭 환경을 제공하는 솔루션인 톡피넛을 개발하려고 합니다. 기존에도 발표 연습 도우미와 같은 서비스들은 존재합니다. 그러나 이런 서비스들은 스피치 교정이라기 보다는 연습 보조에 치우쳐 있습니다. 스피치 학원같이 전문가의 코칭을 받는 방법도 있지만 설문조사 결과 사용자들은 이런 코칭에 부담감을 느끼는 것으로 나타났습니다. 또한 많은 사용자들이 스피치를 혼자 녹음한 후 다시 들어보며 연습한다고 응답했기 때문에, 저희는 그 과정에 필요한 도구를 제공하고 보완점을 교정해주며, 나아가 전문가의 도움을 부담 없이 받도록 해주는 솔루션을 제공하고자 합니다. 저희가 생각한 기능은 크게 스피치 녹음 및 피드백, 교정 표시와 사용자 기호, 스피치 전문가 매칭으로 나뉩니다. 이에 대해서는 뒤에서 자세히 설명드리겠습니다. 프로젝트 중에는 최대한 사용자들의 의견을 많이 반영하려고 합니다. 다양한 스피치 분야 중 특히 발표에 특화된 솔루션을 먼저 제작해 8월과 10월에 총 두 번 베타테스트를 진행할 예정입니다. 사용자의 피드백을 받아 기능을 개선한 후 다른 분야의 스피치도 지원하도록 수평적으로 확장할 계획입니다. 결과물은 웹 서비스와 하이브리드 앱으로 생각하고 있고, 솔루션에 대해 특허 출원을 할 것입니다. 저희 솔루션을 통해 스피치 교정에 대한 진입 장벽이 완화되어 잠재 고객이 스피치 교정 시장의 고객으로 전환될 수 있을 것입니다. 기획에 앞서 설문조사를 진행해 보았습니다. 총 91.2%의 사용자가 말하기 실력 향상을 위해 따로 시간을 들여 노력해 본 경험이 있다고 답했으며, 62.1%의 사용자가 전문가에게 스피치 코칭을 받아 볼 의향은 있으나 경험은 없다고 답했습니다. 스피치 코칭을 주저하는 이유로는 비용의 부담, 시공간적 제약, 심리적 부담감을 꼽았습니다. 가장 많은 응답을 받은 비용의 부담에 관해 조사해 보니 실제 스피치 학원의 수업료는 1회 20만원으로 금전적 부담이 컸습니다. 또한 대부분 대면 수업으로 진행되므로 시공간적 제약도 항상 존재했습니다. 스피치 학원 대신, 사용자들은 주로 혼자 발표 내용을 녹음해 들어보며 연습하는 것으로 나타났습니다. 그 과정에서 예상되는 어려움과 그를 해결하기 위한 기능에 대해 사용자가 얼마나 긍정적인 반응을 보일 지 조사해 보았습니다. 보다시피 대부분의 사용자가 저희가 제공하고자 하는 기능에 긍정적인 반응을 보였습니다. 지금까지 설명드린 설문조사 결과에서, 저희는 효과적이고 효율적인 스피치 연습을 가능하게 하고, 스피치 교정에 대한 사용자의 부담을 덜 수 있는 솔루션의 필요성을 확인했습니다.".split(
+      " "
+    );
+  // const text = "저희는 음성 데이터 분석을 통해".split(" ");
   const started = [3, 5, 8, 11, 13]; // 단어별 시작 시간
   started.unshift(0);
   // 각 기호의 렌더링 여부
@@ -132,6 +161,7 @@ const Speech = () => {
   const [pauseSymbol, setPauseSymbol] = useState(text.map(() => false));
   const [mouseSymbol, setMouseSymbol] = useState(text.map(() => false));
   const [slashSymbol, setSlashSymbol] = useState(text.map(() => false));
+  const [highlighted, setHighlighted] = useState(text.map(() => ""));
 
   const { count, start, stop, reset } = useCounter(0, 1000); // 1초 단위 타이머
 
@@ -141,6 +171,18 @@ const Speech = () => {
     // console.log("selectedSymbol: ", selectedSymbol);
     // console.log("selectedWordIdx: ", selectedWordIdx);
     switch (selectedSymbol) {
+      case "0":
+        highlighted[selectedWordIdx] = "yellow";
+        setHighlighted([...highlighted]);
+        break;
+      case "1":
+        highlighted[selectedWordIdx] = "pink";
+        setHighlighted([...highlighted]);
+        break;
+      case "2":
+        highlighted[selectedWordIdx] = "yellowgreen";
+        setHighlighted([...highlighted]);
+        break;
       case "4":
         enterSymbol[selectedWordIdx] = true;
         setEnterSymbol([...enterSymbol]);
@@ -157,6 +199,18 @@ const Speech = () => {
         slashSymbol[selectedWordIdx] = true;
         setSlashSymbol([...slashSymbol]);
         break;
+      case "8":
+        enterSymbol[selectedWordIdx] = false;
+        setEnterSymbol([...enterSymbol]);
+        pauseSymbol[selectedWordIdx] = false;
+        setPauseSymbol([...pauseSymbol]);
+        mouseSymbol[selectedWordIdx] = false;
+        setMouseSymbol([...mouseSymbol]);
+        slashSymbol[selectedWordIdx] = false;
+        setSlashSymbol([...slashSymbol]);
+        highlighted[selectedWordIdx] = "";
+        setHighlighted([...highlighted]);
+        break;
       default:
         break;
     }
@@ -168,14 +222,25 @@ const Speech = () => {
       <Container cursor={cursor}>
         <Tools>
           {symbols.map((c, i) => (
-            <Tool key={i} id={i} src={c} cursor={cursor} onClick={clickTool} />
+            <ToolKit
+              key={i}
+              id={i}
+              src={c}
+              cursor={cursor}
+              onClick={clickTool}
+            />
           ))}
         </Tools>
         <div style={{ display: "flex", flexDirection: "column" }}>
           <ScriptContainer>
             {text.map((word, i) =>
               started[i] < count ? (
-                <PlayedText onClick={clickWord} key={i} id={i}>
+                <PlayedText
+                  color={highlighted[i]}
+                  onClick={clickWord}
+                  key={i}
+                  id={i}
+                >
                   {enterSymbol[i] ? <Tool src={enter} /> : null}
                   {pauseSymbol[i] ? <Tool src={pause} /> : null}
                   {mouseSymbol[i] ? <Tool src={mouse} /> : null}
@@ -183,7 +248,7 @@ const Speech = () => {
                   {word}
                 </PlayedText>
               ) : (
-                <Text onClick={clickWord} key={i} id={i}>
+                <Text color={highlighted[i]} onClick={clickWord} key={i} id={i}>
                   {enterSymbol[i] ? <Tool src={enter} /> : null}
                   {pauseSymbol[i] ? <Tool src={pause} /> : null}
                   {mouseSymbol[i] ? <Tool src={mouse} /> : null}
@@ -199,7 +264,7 @@ const Speech = () => {
             <button onClick={reset}>reset</button>
           </div>
         </div>
-        {/* <Link to="/presentation/practice">연습 시작</Link> */}
+        <Link to="/presentation/practice">연습 시작</Link>
       </Container>
     </>
   );
