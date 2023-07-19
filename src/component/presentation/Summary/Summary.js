76eda278451d2f6a63aa7ba46d85dc24c3bf8fbe
mkthebea <5596molly@naver.com>
2023-07-20 03:14:01 +0900

    feat: change some graph options and add mock data (#31)

--- src/component/presentation/Summary/Summary.js
+++ src/component/presentation/Summary/Summary.js
@@ -4,7 +4,7 @@ import * as s from "./SummaryStyle";
 import { ResponsiveLine } from "@nivo/line";
 
 const Summary = () => {
-  const MyResponsiveLine = ({ data }) => (
+  const MyResponsiveLine = ({ data, recommended }) => (
     <ResponsiveLine
       data={data}
       margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
@@ -17,7 +17,7 @@ const Summary = () => {
         stacked: true,
         reverse: false,
       }}
-      yFormat=" >-.2f"
+      yFormat=" >-.2~f"
       // axisTop={null}
       // axisRight={null}
       axisBottom={{
@@ -38,15 +38,17 @@ const Summary = () => {
       pointBorderColor={{ from: "serieColor" }}
       enablePointLabel={true}
       pointLabelYOffset={-16}
-      // enableArea={true}
-      // areaBaselineValue={10}
+      enableArea={recommended ? true : false}
+      areaBaselineValue={recommended}
       useMesh={true}
       legends={[]}
     />
   );
-  const data = [
+
+  // mock data
+  const feedback = [
     {
-      id: "feedbacks",
+      id: "feedback",
       color: "hsl(279, 70%, 50%)",
       data: [
         {
@@ -64,6 +66,66 @@ const Summary = () => {
       ],
     },
   ];
+  const speed = [
+    {
+      id: "speed",
+      color: "hsl(279, 70%, 50%)",
+      data: [
+        {
+          x: 1,
+          y: 420,
+        },
+        {
+          x: 2,
+          y: 234,
+        },
+        {
+          x: 3,
+          y: 302,
+        },
+      ],
+    },
+  ];
+  const pause = [
+    {
+      id: "pause",
+      color: "hsl(279, 70%, 50%)",
+      data: [
+        {
+          x: 1,
+          y: 27,
+        },
+        {
+          x: 2,
+          y: 20,
+        },
+        {
+          x: 3,
+          y: 15,
+        },
+      ],
+    },
+  ];
+  const hz = [
+    {
+      id: "hz",
+      color: "hsl(279, 70%, 50%)",
+      data: [
+        {
+          x: 1,
+          y: 420,
+        },
+        {
+          x: 2,
+          y: 390,
+        },
+        {
+          x: 3,
+          y: 300,
+        },
+      ],
+    },
+  ];
   return (
     <>
       <s.Container>
@@ -77,20 +139,26 @@ const Summary = () => {
           <s.SummaryWrapper>
             <s.SummaryText>교정 피드백 수</s.SummaryText>
             <s.Graph>
-              <MyResponsiveLine data={data} />
+              <MyResponsiveLine data={feedback} recommended={null} />
             </s.Graph>
           </s.SummaryWrapper>
           <s.SummaryWrapper>
             <s.SummaryText>평균 속도</s.SummaryText>
-            <s.Graph />
+            <s.Graph>
+              <MyResponsiveLine data={speed} recommended={350} />
+            </s.Graph>
           </s.SummaryWrapper>
           <s.SummaryWrapper>
             <s.SummaryText>평균 휴지</s.SummaryText>
-            <s.Graph />
+            <s.Graph>
+              <MyResponsiveLine data={pause} recommended={25} />
+            </s.Graph>
           </s.SummaryWrapper>
           <s.SummaryWrapper>
             <s.SummaryText>평균 음높이</s.SummaryText>
-            <s.Graph />
+            <s.Graph>
+              <MyResponsiveLine data={hz} recommended={300} />
+            </s.Graph>
           </s.SummaryWrapper>
         </s.SummaryContainer>
       </s.Container>
