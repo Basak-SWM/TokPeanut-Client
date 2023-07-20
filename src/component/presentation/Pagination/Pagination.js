9a9ee8baa72f0aa49077b64511bee904874528f4
mkthebea <5596molly@naver.com>
2023-07-20 16:43:32 +0900

    feat: make pagination style and add to Speech and Summary component (#27)

---
+++ src/component/presentation/Pagination/Pagination.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import * as s from "./PaginationStyle";
+
+const Pagination = () => {
+  return <s.Container>Pagination</s.Container>;
+};
+
+export default Pagination;
---
+++ src/component/presentation/Pagination/PaginationStyle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 200px;
+  height: 500px;
+  border: 1px solid grey;
+  border-radius: 5px;
+  margin: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  box-shadow: 2px 3px 5px 0px grey;
+`;
