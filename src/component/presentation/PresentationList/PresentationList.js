753635c4b061ee9852f000defa4da7760a779a47
mkthebea <5596molly@naver.com>
2023-07-25 16:20:54 +0900

    feat: get presentation list from server (#40)

--- src/component/presentation/PresentationList/PresentationList.js
+++ src/component/presentation/PresentationList/PresentationList.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import * as s from "./PresentationListStyle";
 import axios from "axios";
 
 const PresentationList = () => {
-  axios
-    .get("/presentations", {
-      params: { "account-uuid": "63e11bdb-e4b6-4160-8fdf-b3cd94a0e4c9" },
-    })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // 임시 계정
+  const uuid = "63e11bdb-e4b6-4160-8fdf-b3cd94a0e4c9";
+  const [presentationList, setPresentationList] = useState([]);
+  const getPresentationList = async () => {
+    let res = null;
+    try {
+      res = await axios.get("/presentations", {
+        params: { "account-uuid": uuid },
+      });
+      console.log("presentation list response:", res);
+    } catch (err) {
+      console.log("presentation list error:", err);
+    }
+    setPresentationList(res.data);
+  };
+
+  useEffect(() => {
+    getPresentationList();
+  }, []);
+
   const navigate = useNavigate();
 
   const navigateToPresentation = (e) => {
@@ -21,33 +31,14 @@ const PresentationList = () => {
     navigate(`/presentation/summary?presentation_id=${id}`);
   };
 
-  // mock data
-  const presentationList = [
-    {
-      id: 1,
-      title: "Presentation 1",
-      date: "2023-07-01",
-    },
-    {
-      id: 2,
-      title: "Presentation 2",
-      date: "2023-06-01",
-    },
-    {
-      id: 3,
-      title: "Presentation 3",
-      date: "2023-05-01",
-    },
-  ];
-
   return (
     <>
-      <s.Container id={4}>
+      <s.Container>
         <s.Title>프레젠테이션 목록</s.Title>
         {presentationList.map((p) => (
           <s.Presentation id={p.id} key={p.id} onClick={navigateToPresentation}>
             <s.PresentationTitle>{p.title}</s.PresentationTitle>
-            <s.PresentationDate>{p.date}</s.PresentationDate>
+            <s.PresentationOutline>{p.outline}</s.PresentationOutline>
           </s.Presentation>
         ))}
         <Link to="/presentation/new">새 프레젠테이션</Link>
