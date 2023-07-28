6bc3d4279524a4960206fc6178d6bf515ee9afff
mkthebea <5596molly@naver.com>
2023-07-28 14:12:57 +0900

    feat: global design (#49)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import GlobalStyle from "./style/GlobalStyle";
 import Topbar from "./component/layout/Topbar";
 
 import LandingPage from "./component/LandingPage";
@@ -26,36 +26,39 @@ import Notfound from "./component/Notfound";
 
 function App() {
   return (
-    <Router>
-      <Topbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/presentation" element={<PresentationList />} />
-        <Route path="/presentation/summary" element={<Summary />} />
-        <Route path="/presentation/speech" element={<Speech />} />
-        <Route
-          path="/presentation/practice"
-          element={<Practice isNew={false} />}
-        />
-        <Route path="/presentation/new" element={<NewPresentation />} />
-        <Route
-          path="/presentation/new/practice"
-          element={<Practice isNew={true} />}
-        />
+    <>
+      <GlobalStyle />
+      <Router>
+        <Topbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/presentation" element={<PresentationList />} />
+          <Route path="/presentation/summary" element={<Summary />} />
+          <Route path="/presentation/speech" element={<Speech />} />
+          <Route
+            path="/presentation/practice"
+            element={<Practice isNew={false} />}
+          />
+          <Route path="/presentation/new" element={<NewPresentation />} />
+          <Route
+            path="/presentation/new/practice"
+            element={<Practice isNew={true} />}
+          />
 
-        <Route path="/coach" element={<CoachList />} />
-        <Route path="/coach/portfolio" element={<Portfolio />} />
+          <Route path="/coach" element={<CoachList />} />
+          <Route path="/coach/portfolio" element={<Portfolio />} />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signin/coach" element={<SignInCoach />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signin/coach" element={<SignInCoach />} />
 
-        <Route path="/user/mypage" element={<MyPage />} />
-        <Route path="/user/mymatching" element={<MyMatching />} />
+          <Route path="/user/mypage" element={<MyPage />} />
+          <Route path="/user/mymatching" element={<MyMatching />} />
 
-        <Route path="/*" element={<Notfound />} />
-      </Routes>
-    </Router>
+          <Route path="/*" element={<Notfound />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
