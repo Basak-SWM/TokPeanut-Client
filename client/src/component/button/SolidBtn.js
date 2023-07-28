dbd0f0349249fd9529415c6f6b587e33e652f88e
mkthebea <5596molly@naver.com>
2023-07-28 14:27:54 +0900

    feat: import images and button components, design landing page (#51)

---
+++ client/src/component/button/SolidBtn.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { Box, IconButton, Button } from "@mui/material";
+
+export default function SolidBtn({ text, color }) {
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Pretendard",
+    },
+    palette: {
+      primary: {
+        main: "#FF7134",
+      },
+    },
+  });
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        {color == "white" ? (
+          <SolidButton2 variant="outlined">{text}</SolidButton2>
+        ) : (
+          <SolidButton variant="outlined">{text}</SolidButton>
+        )}
+      </ThemeProvider>
+    </>
+  );
+}
+
+const SolidButton = styled(Button)`
+  background-color: #fff;
+  color: #ff7134;
+  font-size: 2rem;
+  font-weight: 600;
+  box-shadow: none;
+  border-radius: 100px;
+  padding: 0.5rem 3rem;
+  &:hover {
+    background-color: #fff;
+  }
+`;
+
+const SolidButton2 = styled(Button)`
+  background-color: transparent;
+  color: #fff;
+  font-size: 2rem;
+  font-weight: 600;
+  box-shadow: none;
+  border-radius: 100px;
+  padding: 0.5rem 3rem;
+  border-color: #fff;
+  &:hover {
+    border-color: #fff;
+  }
+`;
