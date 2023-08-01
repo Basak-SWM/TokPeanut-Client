bfb3595c92a51977044ebc23a83b4364b0a4179b
mkthebea <5596molly@naver.com>
2023-08-01 17:43:02 +0900

    feat: design summary page and implement speech pagination (#53)

--- client/src/component/presentation/Summary/Summary.js
+++ client/src/component/presentation/Summary/Summary.js
@@ -1,14 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import * as s from "./SummaryStyle";
 import Pagination from "../Pagination/Pagination";
 import { ResponsiveLine } from "@nivo/line";
+import Nav from "../../layout/Nav";
+import axios from "axios";
+import qs from "qs";
+
+import styled from "@emotion/styled";
+import { createTheme, Divider, Icon, ThemeProvider } from "@mui/material";
+import { Box, IconButton, Button } from "@mui/material";
+import theme from "../../../style/theme";
+import Checkbox from "@mui/material/Checkbox";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
+import CircleIcon from "@mui/icons-material/Circle";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const Summary = () => {
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Pretendard",
+    },
+    palette: {
+      primary: {
+        main: "#FF7134",
+      },
+    },
+  });
+
   const MyResponsiveLine = ({ data, recommended, unit }) => (
     <ResponsiveLine
       data={data}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
       xScale={{ type: "point" }}
       xFormat=" >-"
       yScale={{
@@ -19,67 +44,98 @@ const Summary = () => {
         reverse: false,
       }}
       yFormat=" >-.2~f"
-      // axisTop={null}
-      // axisRight={null}
-      // axisBottom={{
-      //   orient: "bottom",
-      //   tickSize: 5,
-      //   tickPadding: 5,
-      //   tickRotation: 0,
-      //   legend: "스피치",
-      //   legendOffset: 36,
-      //   legendPosition: "middle",
-      // }}
-      axisLeft={null}
+      axisBottom={{
+        tickSize: 0,
+        tickPadding: 5,
+        tickRotation: 0,
+      }}
+      axisLeft={{
+        tickSize: 0,
+        tickPadding: 15,
+        tickRotation: 0,
+      }}
       enableGridY={false}
-      isInteractive={false}
-      colors={{ scheme: "nivo" }}
-      pointSize={5}
-      pointColor={{ theme: "background" }}
-      pointBorderWidth={2}
+      enableGridX={false}
+      isInteractive={true}
+      // colors={{ scheme: "nivo" }}
+      colors="#FF7134"
+      lineWidth={1}
+      pointSize={10}
+      pointColor="#FF7134"
+      pointBorderWidth={0}
       pointBorderColor={{ from: "serieColor" }}
       enablePointLabel={true}
       pointLabelYOffset={-16}
       // enableArea={recommended ? true : false}
       // areaBaselineValue={recommended}
       useMesh={true}
+      crosshairType="x"
       legends={[]}
       markers={
         recommended
           ? [
               {
                 axis: "y",
-                value: recommended[0],
-                lineStyle: {
-                  stroke: "grey",
-                  strokeWidth: recommended[0] > 0 ? 0.5 : 0,
-                },
-              },
-              {
-                axis: "y",
                 value: recommended[1],
-                lineStyle: { stroke: "grey", strokeWidth: 0.5 },
+                lineStyle: { stroke: "grey", strokeWidth: 0 },
                 legend: `권장 범위 (${recommended[0]} ~ ${recommended[1]}${unit})`,
                 textStyle: {
                   fill: "grey",
-                  fontSize: 10,
+                  fontSize: 12,
                 },
               },
             ]
           : []
       }
-      // layers 설정 안해주니까 마커 안보임
+      tooltip={({ point }) => {
+        const { id, value, index, data } = point;
+        return (
+          <div
+            style={{
+              background: "white",
+              padding: "8px",
+              border: "1px solid #ccc",
+              fontSize: "12px",
+            }}
+          >
+            <div>
+              {data.x} : {data.y} {unit}
+            </div>
+          </div>
+        );
+      }}
       layers={[
+        // 권장 범위 위에도 호버할 수 있도록 맨 앞에 배치
+        ({ xScale, yScale }) => {
+          if (recommended && recommended.length === 2) {
+            const [min, max] = recommended;
+            const minY = yScale(min);
+            const maxY = yScale(max);
+
+            return (
+              <rect
+                x={0}
+                y={maxY}
+                width={xScale.range()[1] - xScale.range()[0]}
+                height={minY - maxY}
+                fill="#FF7134"
+                fillOpacity={0.1}
+              />
+            );
+          } else {
+            return null;
+          }
+        },
         "grid",
         "axes",
-        "areas",
+        // "areas",
         "crosshair",
         "lines",
         "points",
         "slices",
         "mesh",
         "legends",
-        "markers", // 마커를 제일 뒤에 둬서 위로 오도록
+        "markers",
       ]}
     />
   );
@@ -88,7 +144,6 @@ const Summary = () => {
   const feedback = [
     {
       id: "feedback",
-      color: "hsl(279, 70%, 50%)",
       data: [
         {
           x: "speech 1",
@@ -108,7 +163,6 @@ const Summary = () => {
   const speed = [
     {
       id: "speed",
-      color: "hsl(279, 70%, 50%)",
       data: [
         {
           x: "speech 1",
@@ -128,7 +182,6 @@ const Summary = () => {
   const pause = [
     {
       id: "pause",
-      color: "hsl(279, 70%, 50%)",
       data: [
         {
           x: "speech 1",
@@ -148,7 +201,6 @@ const Summary = () => {
   const hz = [
     {
       id: "hz",
-      color: "hsl(279, 70%, 50%)",
       data: [
         {
           x: "speech 1",
@@ -165,49 +217,277 @@ const Summary = () => {
       ],
     },
   ];
+
+  // 현재 프레젠테이션의 스피치 리스트 받아오기
+  const location = useLocation();
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const presentation_id = query.presentation_id;
+  console.log(presentation_id);
+
+  const [speechList, setSpeechList] = useState([]);
+  const getSpeechList = async () => {
+    let res = null;
+    try {
+      res = await axios.get(`/presentations/${presentation_id}/speeches`);
+      console.log("speech list response:", res);
+    } catch (err) {
+      console.log("speech list error:", err);
+    }
+    setSpeechList(res.data);
+  };
+
+  useEffect(() => {
+    getSpeechList();
+  }, []);
+
+  const navigate = useNavigate();
+
+  const navigateToSpeech = (i) => {
+    navigate(`/presentation/speech?speech_id=${i}`);
+  };
+
   return (
     <>
-      <s.Container>
-        {/* <s.Pagination>
-          (스피치 목록)
-          <Link to="/presentation/speech?speech_id=1">Speech 1</Link>
-          <Link to="/presentation/speech?speech_id=2">Speech 2</Link>
-          <Link to="/presentation/speech?speech_id=3">Speech 3</Link>
-        </s.Pagination> */}
-        <Pagination />
-        <s.SummaryContainer>
-          <s.SummaryWrapper>
-            <s.SummaryText>교정 피드백 수</s.SummaryText>
-            <s.Graph>
-              <MyResponsiveLine data={feedback} recommended={null} />
-            </s.Graph>
-          </s.SummaryWrapper>
-          <s.SummaryWrapper>
-            <s.SummaryText>평균 속도</s.SummaryText>
-            <s.Graph>
-              <MyResponsiveLine
-                data={speed}
-                recommended={[0, 350]}
-                unit="음절/min"
-              />
-            </s.Graph>
-          </s.SummaryWrapper>
-          <s.SummaryWrapper>
-            <s.SummaryText>평균 휴지</s.SummaryText>
-            <s.Graph>
-              <MyResponsiveLine data={pause} recommended={[0, 25]} unit="%" />
-            </s.Graph>
-          </s.SummaryWrapper>
-          <s.SummaryWrapper>
-            <s.SummaryText>평균 음높이</s.SummaryText>
-            <s.Graph>
-              <MyResponsiveLine data={hz} recommended={[200, 400]} unit="Hz" />
-            </s.Graph>
-          </s.SummaryWrapper>
-        </s.SummaryContainer>
-      </s.Container>
+      <ThemeProvider theme={theme}>
+        {/* <Pagination /> */}
+        <Nav />
+        <Container>
+          <div className="title">
+            <h1>프레젠테이션 요약</h1>
+          </div>
+          <Content>
+            {/* 페이지네이션 */}
+            <div className="list-box">
+              <h2>프레젠테이션 목록</h2>
+              <ul className="prsentaition-list">
+                {speechList.map((speech, i) => (
+                  <li key={i}>
+                    <OutlinedBtn
+                      variant="outlined"
+                      onClick={() => navigateToSpeech(speech.id)}
+                    >
+                      <Checkbox
+                        {...label}
+                        icon={<StarBorderIcon />}
+                        checkedIcon={<StarIcon />}
+                        checked
+                      />
+                      <div className="name">
+                        <h3>Speech {i + 1}</h3>
+                        <p>날짜 필요</p>
+                      </div>
+                    </OutlinedBtn>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* 그래프 */}
+            <div className="graph-box">
+              <ul className="graph-wrap">
+                <li>
+                  <div className="sub-title">
+                    <CircleIcon />
+                    <h3>교정 피드백 수</h3>
+                  </div>
+                  <div className="graph">
+                    <GraphBox>
+                      <MyResponsiveLine
+                        data={feedback}
+                        recommended={null}
+                        unit={"개"}
+                      />
+                    </GraphBox>
+                  </div>
+                </li>
+                <li>
+                  <div className="sub-title">
+                    <CircleIcon />
+                    <h3>평균속도</h3>
+                  </div>
+                  <div className="graph">
+                    <GraphBox>
+                      <MyResponsiveLine
+                        data={speed}
+                        recommended={[0, 350]}
+                        unit="음절/min"
+                      />
+                    </GraphBox>
+                  </div>
+                </li>
+                <li>
+                  <div className="sub-title">
+                    <CircleIcon />
+                    <h3>평균 휴지</h3>
+                  </div>
+                  <div className="graph">
+                    <GraphBox>
+                      <MyResponsiveLine
+                        data={pause}
+                        recommended={[0, 25]}
+                        unit="%"
+                      />
+                    </GraphBox>
+                  </div>
+                </li>
+                <li>
+                  <div className="sub-title">
+                    <CircleIcon />
+                    <h3>평균 음높이</h3>
+                  </div>
+                  <div className="graph">
+                    <GraphBox>
+                      <MyResponsiveLine
+                        data={hz}
+                        recommended={[200, 400]}
+                        unit="Hz"
+                      />
+                    </GraphBox>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </Content>
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
+
+const Container = styled(Box)`
+  width: 118rem;
+  margin: 13rem auto 10rem auto;
+  .title {
+    padding-bottom: 1rem;
+    border-bottom: 2px solid #ff7134;
+    h1 {
+      font-size: 2.5rem;
+      color: #3b3b3b;
+      font-weight: 700;
+      line-height: 150%;
+    }
+  }
+  @media ${() => theme.device.desktop} {
+    width: 90%;
+  }
+  @media ${() => theme.device.tablet} {
+  }
+`;
+
+const Content = styled(Box)`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin-top: 3rem;
+  .list-box {
+    width: 24%;
+    h2 {
+      font-size: 1.8rem;
+      color: #3b3b3b;
+      line-height: 150%;
+      margin-bottom: 2rem;
+      font-weight: 500;
+    }
+    .prsentaition-list {
+      li {
+        margin-bottom: 1rem;
+        .Mui-checked {
+          color: #fce87e;
+        }
+      }
+      li:last-of-type {
+        margin: 0;
+      }
+    }
+  }
+  .graph-box {
+    width: 74%;
+    .graph-wrap {
+      li {
+        margin-bottom: 3rem;
+        .sub-title {
+          display: flex;
+          align-items: center;
+          svg {
+            color: #ff7134;
+            width: 2rem;
+            height: 2rem;
+            margin-right: 1rem;
+          }
+          h3 {
+            font-size: 1.8rem;
+            color: #3b3b3b;
+            line-height: 150%;
+            font-weight: 600;
+          }
+        }
+        .graph {
+          padding: 2rem;
+          border: 1px solid rgba(0, 0, 0, 0.1);
+          border-radius: 1rem;
+          margin-top: 2rem;
+        }
+      }
+      li:last-of-type {
+        margin-bottom: 0;
+      }
+    }
+  }
+  @media ${() => theme.device.tablet} {
+    flex-direction: column;
+    .list-box {
+      width: 100%;
+      height: 35rem;
+      max-height: 35rem;
+      overflow-y: scroll;
+    }
+    .graph-box {
+      width: 100%;
+      margin-top: 3rem;
+    }
+  }
+`;
+
+const OutlinedBtn = styled(Button)`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 2rem;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  svg {
+    width: 3rem;
+    height: 3rem;
+  }
+  .name {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 1rem;
+    h3 {
+      font-size: 2rem;
+      font-weight: 600;
+      line-height: 150%;
+      color: #3b3b3b;
+    }
+    p {
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 1.4rem;
+      line-height: 150%;
+      font-family: 400;
+    }
+  }
+  &:hover {
+    background-color: #fff;
+  }
+`;
+
+const GraphBox = styled(Box)`
+  position: relative;
+  padding-bottom: 0;
+  height: 40rem;
+`;
 
 export default Summary;
