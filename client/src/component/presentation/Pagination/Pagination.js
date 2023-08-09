ef2f5725355f4ba26923d4af07732f29b262f666
mkthebea <5596molly@naver.com>
2023-08-09 21:14:14 +0900

    fix: show text before waveform loaded, preventing a script click when the waveform is not loaded

--- client/src/component/presentation/Pagination/Pagination.js
+++ client/src/component/presentation/Pagination/Pagination.js
@@ -129,7 +129,7 @@ const PageBtnWrap = styled(Box)`
   right: 3%;
   ul {
     max-height: 80vh;
-    /* overflow-y: scroll; */
+    overflow-y: scroll;
     background-color: #fff;
     border: 1px solid rgba(0, 0, 0, 0.1);
     margin-top: 13rem;
