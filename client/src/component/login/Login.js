5ddf538d294d7ff7fc74b085f02ae6fcdb1315ea
mkthebea <5596molly@naver.com>
2023-09-04 16:35:33 +0900

    feat: login page and join modal (#87)

--- client/src/component/login/Login.js
+++ client/src/component/login/Login.js
@@ -1,13 +1,165 @@
-import React from "react";
-import NotReady from "../NotReady";
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { createTheme, Divider, Icon, ThemeProvider } from "@mui/material";
+import { createGlobalStyle } from "styled-components";
+import { Box, IconButton, Button } from "@mui/material";
+import Nav from "../layout/Nav";
+import theme from "../../style/theme";
+import TextField from "@mui/material/TextField";
+import JoinModal from "../modal/JoinModal";
 
 const Login = () => {
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Pretendard",
+    },
+    palette: {
+      primary: {
+        main: "#FF7134",
+      },
+      secondary: {
+        main: "#D9D9D9",
+      },
+    },
+  });
   return (
     <>
-      <h1>Login / 로그인 화면</h1>
-      <NotReady />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Nav />
+        <LoginWrap>
+          <LoginBox>
+            <PaddingWrap>
+              <h1>LOGIN</h1>
+              <div className="input-wrap">
+                <StyledTextField
+                  id="id"
+                  variant="outlined"
+                  type="text"
+                  placeholder="아이디를 입력하세요"
+                  fullWidth
+                />
+                <StyledTextField
+                  id="pw"
+                  variant="outlined"
+                  type="password"
+                  placeholder="비밀번호를 입력하세요"
+                  fullWidth
+                />
+              </div>
+              <div className="join-text">
+                <h3>톡피넛이 처음이신가요?</h3>
+                <JoinModal />
+              </div>
+              <LoginBtn variant="contained" fullWidth>
+                로그인
+              </LoginBtn>
+              <Divider />
+              <div className="social-login">
+                <OutlinedBtn variant="outlined" color="secondary" fullWidth>
+                  <img src="/img/logo/google.svg" />
+                  구글로 시작하기
+                </OutlinedBtn>
+                <OutlinedBtn variant="outlined" color="secondary" fullWidth>
+                  <img src="/img/logo/kakao.svg" />
+                  카카오로 시작하기
+                </OutlinedBtn>
+              </div>
+            </PaddingWrap>
+          </LoginBox>
+        </LoginWrap>
+      </ThemeProvider>
     </>
   );
 };
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        background-color: #FAFAFA;
+    }
+`;
+
+const LoginWrap = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const LoginBox = styled(Box)`
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px 10px rgba(0, 0, 0, 0.05);
+  margin-top: 8rem;
+  h1 {
+    font-size: 4rem;
+    color: #ff7134;
+    font-weight: bold;
+    line-height: 150%;
+    margin-bottom: 5rem;
+    text-align: center;
+  }
+  .input-wrap {
+    & > div {
+      margin-bottom: 1rem;
+    }
+  }
+  .join-text {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 1rem;
+    h3 {
+      font-size: 1.4rem;
+      color: #3b3b3b;
+      line-height: 150%;
+    }
+  }
+  .social-login {
+    button:first-of-type {
+      margin: 2rem 0 1rem 0;
+    }
+  }
+  @media ${() => theme.device.mobile} {
+    margin-top: 0;
+    box-shadow: none;
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+const PaddingWrap = styled(Box)`
+  padding: 10rem 8rem;
+  @media ${() => theme.device.mobile} {
+    padding: 10rem 2rem 0 2rem;
+  }
+`;
+
+const LoginBtn = styled(Button)`
+  box-shadow: none;
+  font-size: 1.8rem;
+  color: #fff;
+  padding: 1.2rem 2rem;
+  margin: 2rem 0;
+`;
+
+const OutlinedBtn = styled(Button)`
+  color: #3b3b3b;
+  padding: 1.2rem 2rem;
+  font-size: 1.8rem;
+  display: flex;
+  align-items: center;
+  img {
+    margin-right: 1rem;
+  }
+`;
+
+const StyledTextField = styled(TextField)`
+  input {
+    font-size: 1.6rem;
+    color: #3b3b3b;
+    padding: 1.2rem 2rem;
+  }
+`;
 
 export default Login;
---
+++ client/src/component/modal/JoinModal.js
@@ -0,0 +1,354 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { Box, IconButton, Button } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import { createTheme, Divider, Icon, ThemeProvider } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import TextField from "@mui/material/TextField";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import theme from "../../style/theme";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+export default function JoinModal() {
+  const [open, setOpen] = React.useState(false);
+  const [coach, setCoach] = React.useState(false);
+  const [select, setSelect] = React.useState("");
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  const handleChange = (event) => {
+    setSelect(event.target.value);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setCoach(false);
+  };
+
+  const handleCoach = () => {
+    setCoach(!coach);
+  };
+
+  const theme2 = createTheme({
+    typography: {
+      fontFamily: "Pretendard",
+    },
+    palette: {
+      primary: {
+        main: "#FF7134",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme2}>
+      <JoinModalWrap>
+        <ModalBtn onClick={handleClickOpen}>회원가입</ModalBtn>
+        <StyledDialog
+          fullScreen={fullScreen}
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <JoinContent>
+            <div className="title">
+              {!coach ? <h1>회원가입</h1> : <h1>코치 회원가입</h1>}
+              <IconButton onClick={handleClose}>
+                <CloseIcon />
+              </IconButton>
+            </div>
+            <div className="select-option">
+              {!coach ? (
+                <Button onClick={handleCoach}>코치로 가입하시나요?</Button>
+              ) : (
+                <></>
+              )}
+            </div>
+            <div className="join-input-wrap">
+              <ul className="input-wrap">
+                <li>
+                  <h3>이메일</h3>
+                  <div className="input-btn-wrap">
+                    <StyledTextField
+                      id="id"
+                      variant="outlined"
+                      type="text"
+                      placeholder="아이디를 입력하세요"
+                      fullWidth
+                    />
+                    <Button variant="contained">인증</Button>
+                  </div>
+                </li>
+                <li>
+                  <h3>비밀번호</h3>
+                  <StyledTextField
+                    id="pw"
+                    variant="outlined"
+                    type="password"
+                    placeholder="비밀번호를 입력하세요"
+                    fullWidth
+                  />
+                </li>
+                <li>
+                  <h3>비밀번호 확인</h3>
+                  <StyledTextField
+                    id="pw2"
+                    variant="outlined"
+                    type="password"
+                    placeholder="비밀번호를 입력하세요"
+                    fullWidth
+                  />
+                </li>
+                <li>
+                  <h3>전화번호</h3>
+                  <StyledTextField
+                    id="number"
+                    variant="outlined"
+                    type="text"
+                    placeholder="'-'제외하고 입력하세요"
+                    fullWidth
+                  />
+                </li>
+                {!coach ? (
+                  <></>
+                ) : (
+                  <>
+                    <li>
+                      <h3>닉네임</h3>
+                      <div className="input-btn-wrap">
+                        <StyledTextField
+                          id="id"
+                          variant="outlined"
+                          type="text"
+                          placeholder="아이디를 입력하세요"
+                          fullWidth
+                        />
+                        <Button variant="contained">중복확인</Button>
+                      </div>
+                    </li>
+                    <li>
+                      <h3>코칭 분야 선택</h3>
+                      <FormControl fullWidth>
+                        <StyledSelect
+                          value={select}
+                          onChange={handleChange}
+                          displayEmpty
+                          inputProps={{ "aria-label": "Without label" }}
+                        >
+                          <StyledMenuItem value="">
+                            <em>선택</em>
+                          </StyledMenuItem>
+                          <StyledMenuItem value={10}>분야</StyledMenuItem>
+                          <StyledMenuItem value={20}>분야</StyledMenuItem>
+                          <StyledMenuItem value={30}>분야</StyledMenuItem>
+                        </StyledSelect>
+                      </FormControl>
+                    </li>
+                  </>
+                )}
+              </ul>
+              <ul className="checkbox-wrap">
+                <li>
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked size="large" />}
+                    label="이용약관에 동의합니다."
+                  />
+                  <a href="">이용약관</a>
+                </li>
+                {!coach ? (
+                  <li>
+                    <FormControlLabel
+                      control={<Checkbox defaultChecked size="large" />}
+                      label="서비스 품질 향상을 위한 스피치 데이터 사용을 허가 합니다."
+                    />
+                    <IconButton>
+                      <ErrorOutlineIcon />
+                    </IconButton>
+                  </li>
+                ) : (
+                  <></>
+                )}
+              </ul>
+            </div>
+            <JoinBtn variant="contained">회원가입</JoinBtn>
+          </JoinContent>
+        </StyledDialog>
+      </JoinModalWrap>
+    </ThemeProvider>
+  );
+}
+
+const JoinModalWrap = styled(Box)``;
+
+const StyledDialog = styled(Dialog)`
+  .MuiPaper-root {
+    width: 75rem;
+    max-width: 75rem;
+  }
+`;
+
+const ModalBtn = styled(Button)`
+  font-size: 1.4rem;
+  color: #ff7134;
+  text-decoration: underline;
+`;
+
+const JoinContent = styled(DialogContent)`
+  padding: 7rem 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .title {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-bottom: 2rem;
+    margin-bottom: 2rem;
+    border-bottom: 2px solid #ff7134;
+    h1 {
+      font-size: 2rem;
+      color: #ff7134;
+      font-weight: 600;
+    }
+    svg {
+      width: 2rem;
+      height: 2rem;
+    }
+  }
+  .select-option {
+    margin-bottom: 2rem;
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-end;
+    width: 100%;
+    button {
+      font-size: 1.6rem;
+      font-weight: 700;
+      color: #ff7134;
+      line-height: 150%;
+      text-decoration: underline;
+      text-align: right;
+      cursor: pointer;
+    }
+  }
+  .join-input-wrap {
+    width: 100%;
+    margin-bottom: 3rem;
+    .input-wrap {
+      margin-bottom: 3rem;
+      li {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        margin-bottom: 2rem;
+        h3 {
+          width: 18rem;
+          font-size: 1.6rem;
+          color: #3b3b3b;
+          font-weight: 600;
+        }
+        .input-btn-wrap {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          width: 100%;
+          button {
+            color: #fff;
+            font-size: 1.6rem;
+            font-weight: 600;
+            box-shadow: none;
+            padding: 1rem 2rem;
+            width: 15rem;
+            margin-left: 1rem;
+          }
+        }
+      }
+    }
+    .checkbox-wrap {
+      padding-bottom: 3rem;
+      border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+      li {
+        padding: 0.8rem 2rem;
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        border-radius: 4px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .MuiFormControlLabel-label {
+          font-size: 1.6rem;
+          color: #3b3b3b;
+        }
+        a {
+          font-size: 1.4rem;
+          color: #838383;
+          font-weight: 500;
+          text-decoration: underline;
+        }
+        button {
+          svg {
+            width: 2rem;
+            height: 2rem;
+          }
+        }
+      }
+      li:first-of-type {
+        margin-bottom: 1rem;
+      }
+    }
+  }
+  @media ${() => theme.device.mobile} {
+    justify-content: flex-start;
+    padding: 2rem;
+    .join-input-wrap {
+      .input-wrap {
+        li {
+          flex-direction: column;
+          align-items: flex-start;
+          h3 {
+            margin-bottom: 1rem;
+          }
+        }
+      }
+    }
+  }
+`;
+
+const StyledTextField = styled(TextField)`
+  input {
+    font-size: 16px;
+    color: #3b3b3b;
+    padding: 1.2rem 2rem;
+  }
+`;
+
+const JoinBtn = styled(Button)`
+  box-shadow: none;
+  color: #fff;
+  font-size: 1.8rem;
+  padding: 1rem 2rem;
+  width: 38rem;
+  @media ${() => theme.device.mobile} {
+    width: 100%;
+  }
+`;
+
+const StyledSelect = styled(Select)`
+  font-size: 1.4rem;
+`;
+
+const StyledMenuItem = styled(MenuItem)`
+  font-size: 1.4rem;
+`;
